d4e12f3861c0302ee6483cede71d779db842359a
Brianna Escobar <briannaescobar6@gmail.com>
2023-05-09 14:40:24 -0400

    c#

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Console.WriteLine("Hello, World!");
+
+int age = 19; 
+string name = "Brianna";
+bool isHere = true;
+double salary = 220000.01;
+Console.WriteLine($"Hi, {name}. Your age is {19} and is here? {isHere}");
+Console.WriteLine($"And you make ${salary} a year"); 
